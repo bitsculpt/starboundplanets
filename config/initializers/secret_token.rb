9eafeecfa4161e9998492da3136c035dedcce63b
David Williams <davew1982@gmail.com>
2014-01-11 10:28:55 -0500

    first

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Starboundplanets::Application.config.secret_key_base = '3a5f8535baf16e835d208e1792efa7a43230d58df3acf9a74eef12e753d5042733f012359a5538094c8aa129e979d391f4aeab4fe37aab4b6877f71df9e62358'
