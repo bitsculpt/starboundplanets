c6c2df967df81be08efa605e7ea6fcbceb5a684d
Scott Harman <harmantwo@gmail.com>
2014-01-11 13:22:19 -0500

    add sign in acceptance test

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,13 @@
-# Read about factories at https://github.com/thoughtbot/factory_girl
-
 FactoryGirl.define do
   factory :user do
+    sequence :username do |n|
+      "badassplaya!#{n}"
+    end
+
+    sequence :email do |n|
+      "badass#{n}@playa.com"
+    end
+
+    password "letsplay"
   end
 end
