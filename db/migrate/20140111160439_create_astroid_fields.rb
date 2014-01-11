82462b0089985e7138449a04cdb0ab070f290c0e
Daniel Carter <geddoe@gmail.com>
2014-01-11 12:21:29 -0500

    Null constraints in migration files.

--- db/migrate/20140111160439_create_astroid_fields.rb
+++ db/migrate/20140111160439_create_astroid_fields.rb
@@ -1,9 +1,9 @@
 class CreateAstroidFields < ActiveRecord::Migration
   def change
     create_table :astroid_fields do |t|
-      t.string :name
-      t.integer :cluster_id
-      t.integer :threat_level
+      t.string :name, null:false
+      t.integer :cluster_id, null:false
+      t.integer :threat_level, null:false
       t.text :description
       t.string :username
 
--- db/migrate/20140111160506_create_moons.rb
+++ db/migrate/20140111160506_create_moons.rb
@@ -1,10 +1,10 @@
 class CreateMoons < ActiveRecord::Migration
   def change
     create_table :moons do |t|
-      t.string :name
-      t.integer :cluster_id
-      t.string :biome
-      t.integer :threat_level
+      t.string :name, null:false
+      t.integer :cluster_id, null:false
+      t.string :biome, null:false
+      t.integer :threat_level, null:false
       t.string :username
       t.text :description
 
