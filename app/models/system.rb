b909e811aaf7393c31217b868617cb841f322b77
Daniel Carter <geddoe@gmail.com>
2014-01-11 17:38:48 -0500

    stuff

--- app/models/system.rb
+++ app/models/system.rb
@@ -14,4 +14,7 @@ class System < ActiveRecord::Base
   validates_presence_of :x_coord
   validates_presence_of :y_coord
   validates_presence_of :sector
+
+  validates :x_coord, :numericality => {:only_integer => true}
+  validates :y_coord, :numericality => {:only_integer => true}
 end
