72b9b7be42a52bb9c2b61562132c5c3d2f50d0c6
Daniel Carter <geddoe@gmail.com>
2014-01-11 19:27:37 -0500

    Green acceptance tests for asteroid fields

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Starboundplanets::Application.routes.draw do
   devise_for :users
-  resources :sectors, :systems, :clusters
+  resources :sectors, :systems, :clusters, :asteroid_fields
 
   get 'sector/:sector_id/new', to: 'systems#new', as: 'new_sector_system'
 
   get 'system/:system_id/new', to: 'clusters#new', as: 'new_system_cluster'
+
+  get 'cluster/:cluster_id/new_asteroid', to: 'asteroid_fields#new', as: 'new_cluster_asteroid_field'
 
   root 'home#welcome'
 end
--- spec/factories/asteroid_fields.rb
+++ spec/factories/asteroid_fields.rb
@@ -1,7 +1,7 @@
 # Read about factories at https://github.com/thoughtbot/factory_girl
 
 FactoryGirl.define do
-  factory :astroid_field do
+  factory :asteroid_field do
     name "Thoridon Celsiuc Klingon a"
     threat_level 1
     description "MyText"
