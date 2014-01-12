485e416b1df0a06e353e7c333e40273d689a1532
David Williams <davew1982@gmail.com>
2014-01-11 20:35:26 -0500

    Planet Tests are green

---
+++ app/models/space_junk.rb
@@ -0,0 +1,9 @@
+class SpaceJunk
+  def self.biomes
+    ['Arid','Barren','Desert','Forest','Grassland','Jungle','Magma','Moon','Snow','Tundra','Volcano']
+  end
+
+  def self.threat_levels
+    (1..10).to_a
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Starboundplanets::Application.routes.draw do
   devise_for :users
-  resources :sectors, :systems, :clusters
+  resources :sectors, :systems, :clusters, :planets
 
   get 'sector/:sector_id/new', to: 'systems#new', as: 'new_sector_system'
 
   get 'system/:system_id/new', to: 'clusters#new', as: 'new_system_cluster'
+
+  get 'cluster/:cluster_id/new_planet', to: 'planets#new', as: 'new_cluster_planet'
+
 
   root 'home#welcome'
 end
