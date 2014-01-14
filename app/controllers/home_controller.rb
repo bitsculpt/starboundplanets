7ce7b9843c8e13913400da92cb75c53c9d8f9b25
David Williams <davew1982@gmail.com>
2014-01-14 01:44:58 -0500

    Logged in users can now see last 4 planets and also add new planets from index

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,10 @@
 class HomeController < ApplicationController
-
+# need to refactor this controller and maybe some routes
   def welcome
     if current_user
+      @planets = Planet.all.order(created_at: :asc).limit(4)
+      @moons =   Moon.all.order(created_at: :asc).limit(4)
+      @asteroid_fields = AsteroidField.all.order(created_at: :asc).limit(4)
       render action: :home
     else
       @planets = Planet.all.order(created_at: :asc).limit(4)
