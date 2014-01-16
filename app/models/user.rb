13e96380a17710f998cc387c07e0fa26af60e895
Daniel Carter <geddoe@gmail.com>
2014-01-16 16:04:50 -0500

    Added more styling. Fixed logging in with username.

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,5 +8,15 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable,
+         :validatable, :authentication_keys => [:username]
+
+  def self.find_first_by_auth_conditions(warden_conditions)
+    conditions = warden_conditions.dup
+    if login = conditions.delete(:username)
+      where(conditions).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+    else
+      where(conditions).first
+    end
+  end
 end
