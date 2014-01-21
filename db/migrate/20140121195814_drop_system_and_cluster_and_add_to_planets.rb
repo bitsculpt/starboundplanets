ffcb77b445cadaa87ce1b10224615b7ecf3a7b21
Daniel Carter <geddoe@gmail.com>
2014-01-21 15:02:55 -0500

    Migration file

---
+++ db/migrate/20140121195814_drop_system_and_cluster_and_add_to_planets.rb
@@ -0,0 +1,35 @@
+class DropSystemAndClusterAndAddToPlanets < ActiveRecord::Migration
+
+  def up
+    drop_table :systems
+    drop_table :clusters
+
+    add_column :planets, :system, :string
+    add_column :planets, :orbit, :string
+    add_column :planets, :x_coord, :integer
+    add_column :planets, :y_coord, :integer
+  end
+
+  def down
+    create_table :systems do |t|
+      t.string :name, null: false
+      t.integer :x_coord, null: false
+      t.integer :y_coord, null: false
+      t.integer :sector_id, null: false
+
+      t.timestamps
+    end
+    create_table :clusters do |t|
+      t.string :name, null: false
+      t.integer :system_id, null: false
+
+      t.timestamps
+    end
+
+    remove_column :planets, :system
+    remove_column :planets, :orbit
+    remove_column :planets, :x_coord
+    remove_column :planets, :y_coord
+  end
+
+end
